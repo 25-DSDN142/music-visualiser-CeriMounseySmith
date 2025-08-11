d90064a9603345ef2df38f97c559fcb54b1745af
CeriMounseySmith <cerimounseysmith@gmail.com>
2025-08-12 11:11:20 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -5,21 +5,7 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
-colorMode(HSB,100,);
-background(255);
-rectMode(CENTER);
-strokeWeight(9);
-stroke(drum,80,80);
-//strokeFill(255);
 
-var drumMap =map(drum,0,100,30,90);
-var lengthOfLine = 300;
-var LineStart = 100;
-var LineEnd= LineStart+ lengthOfLine;
-
-line(LineStart,300, LineEnd, 300);
-
-return;
 
 }
 
