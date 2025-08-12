556943856cd1a766ea97f7100e0f7fa9152496b6
CeriMounseySmith <cerimounseysmith@gmail.com>
2025-08-12 13:55:28 +1200

    Replaced music with Mojang-Clark
    Island living music didn't work well, replaced with a different song that picks up better.

--- music_starter.js
+++ music_starter.js
@@ -6,43 +6,42 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
-ellipse(200,200,40,40);
-ellipse(250,250,50,50)
-ellipse(310,310,60,60)
-ellipse(380,380,70,70)
+// ellipse(200,200,40,40);
+// ellipse(250,250,50,50)
+// ellipse(310,310,60,60)
+// ellipse(380,380,70,70)
 
 ///////////////////////stuff that was already here
-
-// let bar_spacing = height / 10; // space between each height
-// let bar_height = width / 12;   //width of bar
-// let bar_pos_x = width / 2;     // bar position
+let bar_spacing = height / 10; // space between each height
+let bar_height = width / 12;   //width of bar
+let bar_pos_x = width / 2;     // bar position
  
    
 // // changes 
 //    // // vocal bar is red
-// fill(172, 120, 186);// lavendar
-// rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height); //* is xtimes
-// fill(0);
-// text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+fill(172, 120, 186);// lavendar
+rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height); //* is xtimes
+ fill(0);
+text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
 //    // // drum bar is green
-// fill(235, 195, 106);//light yellow
-// rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-// fill(0);
-// text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+fill(235, 195, 106);//light yellow
+rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+ fill(0);
+ text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
 //    // // bass bar is blue
-// fill(250,158,28);//orange
-// rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-// fill(0);
-// text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+ fill(250,158,28);//orange
+ rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+ fill(0);
+ text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
 //    // // other bar is white
-// fill(250,128,114);//pink
-// rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-// fill(0);
-// text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-// fill(255, 255, 0);
+ fill(250,128,114);//pink
+ rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+ fill(0);
+ text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+ fill(255, 255, 0);
  
 //    // // display "words"
 // textAlign(CENTER); // aligned in center
